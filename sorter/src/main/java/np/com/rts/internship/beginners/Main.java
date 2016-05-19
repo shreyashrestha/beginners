aa3d89c586847e7f5dd67cb67b4b9304496c6755
Shreya Shrestha <shreyashrestha91@gmail.com>
2016-05-19 05:25:47 -0700

    the first question is sorted

--- sorter/src/main/java/np/com/rts/internship/beginners/Main.java
+++ sorter/src/main/java/np/com/rts/internship/beginners/Main.java
@@ -18,11 +18,27 @@ public class Main {
   };
 
   public Main() {
-    // Write your code here
+    for (int a = 0; a < strings.length; a++) {
+
+      for (int b = a + 1; b < strings.length; b++) {
+        if (strings[a].compareTo(strings[b]) < 0) {
+          String temp = strings[b];
+          strings[b] = strings[a];
+          strings[a] = temp;
+        }
 
 
+      }
+
+
+    }
+
+    for (int k = 0; k < strings.length; k++) {
+
+      System.out.println(strings[k]);
+
+    }
   }
-
   public static void main(String[] args) {
     new Main();
   }
